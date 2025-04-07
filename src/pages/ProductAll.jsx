fef5331f5ba30d23ff89a3e0fdcddb9701911578
jeong-minG <ika818488@gmail.com>
2025-04-08 02:18:49 +0900

    2nd commit

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -5,7 +5,7 @@ import { ProductCard } from '../components/ProductCard';
 const ProductAll = () => {
     const [productList, setProductList] = useState([]);
     const getProduct = async () => {
-        let url = `http://localhost:3004/products`;
+        let url = `https://my-json-server.typicode.com/calevv/RC-shoppingmall-demo/products`;
         let response = await fetch(url);
         let data = await response.json();
         setProductList(data);
